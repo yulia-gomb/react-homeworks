e47ac508897a481f6efffc19f37945c50a2c2ecf
Yuliya Grasevich <grasevich.yuliya@student.ehu.lt>
2024-10-29 01:32:16 +0200

    feat-02: add header markup

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,30 @@
-import './Header.css'
+import './Header.css';
+import logoImage from '../../assets/Logo.png';
+import cartImage from '../../assets/cart.png';
 
 function Header() {
 
     return (
-        <div>
-            Header
-        </div>
+        <header className="header">
+
+            <div className="logo">
+                <img src={logoImage} alt="Logo" className="logo-image"/>
+            </div>
+
+
+            <nav className="menu">
+                <a href="#home" className="menu-item">Home</a>
+                <a href="#menu" className="menu-item active-item">Menu</a>
+                <a href="#company" className="menu-item">Company</a>
+                <a href="#login" className="menu-item">Login</a>
+            </nav>
+
+
+            <div className="cart">
+                <img src={cartImage} alt="Cart" className="cart-icon"/>
+                <span className="cart-count">3</span>
+            </div>
+        </header>
     )
 }
 
