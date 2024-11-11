52aed3c91498dfb6866265fd1f8bc1a858d246be
Yuliya Grasevich <grasevich.yuliya@student.ehu.lt>
2024-11-11 23:22:12 +0200

    feat-03: Rewrite the Header component using the class approach

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,31 +1,30 @@
 import './Header.css';
 import logoImage from '../../assets/icons/Logo.png';
 import cartImage from '../../assets/icons/cart.png';
+import { Component } from "react";
 
-function Header() {
+class Header extends Component {
+    render() {
+        return (
+            <header className="header">
+                <div className="logo">
+                    <img src={logoImage} alt="Logo" className="logo-image" />
+                </div>
 
-    return (
-        <header className="header">
+                <nav className="nav">
+                    <a href="#home" className="nav-item">Home</a>
+                    <a href="#menu" className="nav-item active-item">Menu</a>
+                    <a href="#company" className="nav-item">Company</a>
+                    <a href="#login" className="nav-item">Login</a>
+                </nav>
 
-            <div className="logo">
-                <img src={logoImage} alt="Logo" className="logo-image"/>
-            </div>
-
-
-            <nav className="nav">
-                <a href="#home" className="nav-item">Home</a>
-                <a href="#menu" className="nav-item active-item">Menu</a>
-                <a href="#company" className="nav-item">Company</a>
-                <a href="#login" className="nav-item">Login</a>
-            </nav>
-
-
-            <div className="cart">
-                <img src={cartImage} alt="Cart" className="cart-icon"/>
-                <span className="cart-count">3</span>
-            </div>
-        </header>
-    )
+                <div className="cart">
+                    <img src={cartImage} alt="Cart" className="cart-icon" />
+                    <span className="cart-count">3</span>
+                </div>
+            </header>
+        );
+    }
 }
 
 export default Header
