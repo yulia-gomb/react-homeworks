840b44c2c03d5448df58a4ec0b66badaddc804d8
Yuliya Grasevich <grasevich.yuliya@student.ehu.lt>
2024-11-26 00:50:08 +0200

    feat-04: rewrite Button component on functional approach

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,21 +1,14 @@
 import './Button.css'
 import PropTypes from "prop-types";
-import { Component } from "react";
 
-class Button extends Component {
-    render() {
-        const { label, onClick, variant = 'secondary' } = this.props;
 
-        return (
-            <button
-                className={`button ${variant}`}
-                onClick={onClick}
-            >
-                {label}
-            </button>
-        );
-    }
-}
+const Button = ({ label, onClick, variant = "secondary" }) => {
+    return (
+        <button className={`button ${variant}`} onClick={onClick}>
+            {label}
+        </button>
+    );
+};
 
 Button.propTypes = {
     label: PropTypes.string.isRequired,
