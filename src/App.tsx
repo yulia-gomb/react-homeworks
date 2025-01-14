dd26781d4c06cb449e16ed7fa4ad0a59848d81e5
Yuliya Grasevich <grasevich.yuliya@student.ehu.lt>
2025-01-14 14:47:13 +0200

    feat-09: fix app component style

--- src/App.tsx
+++ src/App.tsx
@@ -7,31 +7,30 @@ import { Routes, Route } from 'react-router-dom';
 import NotFoundPage from "./pages/notFoundPage/NotFoundPage";
 import LoginPage from "./pages/loginPage/LoginPage";
 import { selectCartCount } from "./store/cartSlice";
-import { useAppDispatch, useAppSelector } from "./store/hooks";
+import { useAppSelector } from "./store/hooks";
 import OrderPage from "./pages/orderPage/OrderPage";
 import ProtectedRoute from "./pages/ProtectedRoute";
 
 
 const App = () => {
-    const dispatch = useAppDispatch();
-
     const cartCount = useAppSelector(selectCartCount);
 
-
     return (
-        <>
+        <div className="app-container">
             <Header cartCount={cartCount} />
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/menu" element={<MenuPage/>} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route element={<ProtectedRoute />}>
-                    <Route path="/order" element={<OrderPage />} />
-                </Route>
-                <Route path="*" element={<NotFoundPage />} />
-            </Routes>
+            <main className="main-content">
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/menu" element={<MenuPage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route element={<ProtectedRoute />}>
+                        <Route path="/order" element={<OrderPage />} />
+                    </Route>
+                    <Route path="*" element={<NotFoundPage />} />
+                </Routes>
+            </main>
             <Footer />
-        </>
+        </div>
     );
 };
 
