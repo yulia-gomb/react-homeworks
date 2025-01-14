8649603a74a904ffa9a07780dd7acf0c1d590f21
Yuliya Grasevich <grasevich.yuliya@student.ehu.lt>
2025-01-14 14:51:42 +0200

    feat-09: add text if cart is empty

--- src/pages/orderPage/OrderPage.tsx
+++ src/pages/orderPage/OrderPage.tsx
@@ -4,6 +4,7 @@ import { RootState } from '../../store/store';
 import { removeFromCart, updateQuantity } from "../../store/cartSlice";
 import Button from "../../components/button/Button";
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 
 const OrderPage = () => {
@@ -56,79 +57,93 @@ const OrderPage = () => {
         }
     };
 
+    const navigate = useNavigate();
+    const handlePlaceAnOrderClick = (): void => {
+        navigate("/menu");
+    };
+
     return (
         <div className="order-page">
-            <h1>Finish your order</h1>
+            {cartItems.length === 0 ? (
+                <div>
+                    <h1>Your cart is empty</h1>
+                    <Button label="Place an Order" onClick={handlePlaceAnOrderClick} variant="primary"/>
+                </div>
+            ) : (
+                <>
+                    <h1>Finish your order</h1>
 
-            <div className="order-items">
-                {cartItems.map((item) => (
-                    <div key={item.id} className="order-item">
-                        <img src={item.img} alt={item.meal} className="order-item-image"/>
-                        <div className="order-item-details">
-                            <h3>{item.meal}</h3>
-                            <span className="order-item-price">$ {item.price.toFixed(2)} USD</span>
-                        </div>
-                        <div className="order-item-actions">
+                    <div className="order-items">
+                        {cartItems.map((item) => (
+                            <div key={item.id} className="order-item">
+                                <img src={item.img} alt={item.meal} className="order-item-image" />
+                                <div className="order-item-details">
+                                    <h3>{item.meal}</h3>
+                                    <span className="order-item-price">$ {item.price.toFixed(2)} USD</span>
+                                </div>
+                                <div className="order-item-actions">
+                                    <input
+                                        type="number"
+                                        min={1}
+                                        value={item.quantity}
+                                        onChange={(e) => handleQuantityChange(item.id, parseInt(e.target.value, 10))}
+                                        className="order-item-quantity"
+                                    />
+                                    <button
+                                        onClick={() => handleRemoveItem(item.id)}
+                                        className="order-item-remove"
+                                    >
+                                        X
+                                    </button>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+
+                    <div className="order-total">
+                        <h2>Total:</h2>
+                        <span className="order-total-amount">$ {totalAmount.toFixed(2)} USD</span>
+                    </div>
+
+                    <form className="order-form" onSubmit={handleSubmit}>
+                        <div className="form-group">
                             <input
-                                type="number"
-                                min={1}
-                                value={item.quantity}
-                                onChange={(e) => handleQuantityChange(item.id, parseInt(e.target.value, 10))}
-                                className="order-item-quantity"
+                                type="text"
+                                name="street"
+                                placeholder="Street"
+                                value={formData.street}
+                                onChange={handleInputChange}
                             />
-                            <button
-                                onClick={() => handleRemoveItem(item.id)}
-                                className="order-item-remove"
-                            >
-                                X
-                            </button>
+                            <div className="order-error-container">
+                                {errors.street && (
+                                    <span className={`order-error-message ${errors.street ? "visible" : ""}`}>
+                                        {errors.street}
+                                    </span>
+                                )}
+                            </div>
                         </div>
-                    </div>
-                ))}
-            </div>
-
-            <div className="order-total">
-                <h2>Total:</h2>
-                <span className="order-total-amount">$ {totalAmount.toFixed(2)} USD</span>
-            </div>
-
-            <form className="order-form" onSubmit={handleSubmit}>
-                <div className="form-group">
-                    <input
-                        type="text"
-                        name="street"
-                        placeholder="Street"
-                        value={formData.street}
-                        onChange={handleInputChange}
-                    />
-                    <div className="order-error-container">
-                        {errors.street && (
-                            <span className={`order-error-message ${errors.street ? "visible" : ""}`}>
-                                {errors.street}
-                            </span>
-                        )}
-                    </div>
-                    </div>
-                    <div className="form-group">
-                        <input
-                            type="text"
-                            name="house"
-                            placeholder="House"
-                            value={formData.house}
-                            onChange={handleInputChange}
-                        />
-                        <div className="order-error-container">
-                            {errors.house && (
-                                <span className={`order-error-message ${errors.house ? "visible" : ""}`}>
-                                {errors.house}
-                            </span>
-                            )}
+                        <div className="form-group">
+                            <input
+                                type="text"
+                                name="house"
+                                placeholder="House"
+                                value={formData.house}
+                                onChange={handleInputChange}
+                            />
+                            <div className="order-error-container">
+                                {errors.house && (
+                                    <span className={`order-error-message ${errors.house ? "visible" : ""}`}>
+                                        {errors.house}
+                                    </span>
+                                )}
+                            </div>
                         </div>
-                    </div>
-                    <Button label="Order" variant="primary"/>
-            </form>
+                        <Button label="Order" variant="primary" />
+                    </form>
+                </>
+            )}
         </div>
-);
+    );
 };
 
 export default OrderPage;
