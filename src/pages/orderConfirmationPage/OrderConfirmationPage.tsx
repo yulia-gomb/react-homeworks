e9e859c51f5306497b5c458c0bd6b334a34460f6
Yuliya Grasevich <grasevich.yuliya@student.ehu.lt>
2025-01-23 17:16:09 +0200

    feat-10: add dark theme to the OrderConfirmation page

--- src/pages/orderConfirmationPage/OrderConfirmationPage.tsx
+++ src/pages/orderConfirmationPage/OrderConfirmationPage.tsx
@@ -1,13 +1,16 @@
 import './OrderConfirmationPage.css';
 import { useLocation } from "react-router-dom";
+import { useTheme } from "../../utils/themeContext";
 
 
 const OrderConfirmationPage = () => {
     const location = useLocation();
     const { orderId, deliveryAddress } = location.state || {};
+    const { theme } = useTheme();
+    const confirmationClass = theme === 'dark' ? 'confirmation dark-theme' : 'confirmation';
 
     return (
-        <div className="confirmation-page">
+        <div className={confirmationClass}>
             <h1>Order Confirmed!</h1>
             <p>Your order <span>#{orderId}</span> is successfully placed!</p>
             <p>And will be delivered to:</p>
